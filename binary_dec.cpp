0585cf169f6e0eb0b01e9836e96ec7e72b408378
Vishal Gupta <itsvishalhack@gmail.com>
2022-09-08 10:08:10 +0530

    nothing much better

---
+++ binary_dec.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    string bin;
+    cout<<"Enter yiur binary number: ";
+    cin>>bin;
+    for (int i = 0; i < bin.size(); i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
